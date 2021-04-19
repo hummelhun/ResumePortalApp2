fdf19f3ef1846d5c6def6e187b9218293205607e
Pogácsás Csaba <hummelhun@gmail.com>
2021-04-19 17:07:57 +0200

    Adding User JPA infrastructure

---
+++ src/main/java/io/csabap/resumeportal/HomeController.java
@@ -0,0 +1,13 @@
+package io.csabap.resumeportal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello";
+    }
+}
---
+++ src/main/java/io/csabap/resumeportal/UserRepository.java
@@ -0,0 +1,10 @@
+package io.csabap.resumeportal;
+
+import io.csabap.resumeportal.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+}
