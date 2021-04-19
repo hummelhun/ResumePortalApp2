d7b4a143098fbc012ddb4ac30c85a35169e19ead
Pogácsás Csaba <hummelhun@gmail.com>
2021-04-19 18:31:46 +0200

    Added a profile view path + thymeleaf

--- src/main/java/io/csabap/resumeportal/HomeController.java
+++ src/main/java/io/csabap/resumeportal/HomeController.java
@@ -1,9 +1,12 @@
 package io.csabap.resumeportal;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
@@ -13,5 +16,11 @@ public class HomeController {
     @GetMapping("/edit")
     public String edit(){
         return "Edit page";
+    }
+
+    @GetMapping("/view/{userId}")
+    public String view(@PathVariable String userId, Model model){
+        model.addAttribute("userId", userId);
+        return "profile";
     }
 }
